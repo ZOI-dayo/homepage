b351b728c9ef5efeb9c4600f8f966b837d5bc1e2
ZOI-dayo <zoizoi.dayo@gmail.com>
2024-12-30 22:35:59 +0900

    add linter & formatter

---
+++ .prettier.js
@@ -0,0 +1,11 @@
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: "none",
+  semi: false,
+  parser: "typescript",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
